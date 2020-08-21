da1a9a368496dfa5e0765a91e76105484ba392db
aqsakhan22 <47089592+aqsakhan22@users.noreply.github.com>
2020-08-22 00:23:26 +0500

    Updates

---
+++ precache-manifest.9b4fb11a3fc0c6972742b99f1291ef4b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e779ae9182887b127d26126186c7b3",
+    "url": "/aqsakhan22/ReactPractice/index.html"
+  },
+  {
+    "revision": "06e77dc8b65001d95902",
+    "url": "/aqsakhan22/ReactPractice/static/css/2.0514be00.chunk.css"
+  },
+  {
+    "revision": "25ed2ad6420f1739ee17",
+    "url": "/aqsakhan22/ReactPractice/static/css/main.aec8536f.chunk.css"
+  },
+  {
+    "revision": "06e77dc8b65001d95902",
+    "url": "/aqsakhan22/ReactPractice/static/js/2.5d22ace0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aqsakhan22/ReactPractice/static/js/2.5d22ace0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ed2ad6420f1739ee17",
+    "url": "/aqsakhan22/ReactPractice/static/js/main.a9c3b60e.chunk.js"
+  },
+  {
+    "revision": "2eba3a3ef3b807be8cc9",
+    "url": "/aqsakhan22/ReactPractice/static/js/runtime-main.82de2462.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/aqsakhan22/ReactPractice/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/aqsakhan22/ReactPractice/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/aqsakhan22/ReactPractice/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/aqsakhan22/ReactPractice/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/aqsakhan22/ReactPractice/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/aqsakhan22/ReactPractice/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
